bec8832fbd9c28af63a6127ae72ad242b1e013d5
Mason <mason.sciotti@p97.com>
2017-03-29 17:11:51 -0400

    Logging channel is read-only
    Fixed the async/await on channel creation

--- logging.js
+++ logging.js
@@ -1,33 +1,44 @@
 module.exports = {
     logUserBan: function(bot, user, guild) {
-        var loggingChannel = this.getLoggingChannel(guild);
-        loggingChannel.sendMessage(`:skull: **User Banned** :skull:
-                                    \n**User**: ${user.username}`);
+        this.getLoggingChannel(message.guild, function(loggingChannel) {
+            loggingChannel.sendMessage(`:skull: **User Banned** :skull:
+                                       \n**User**: ${user.username}`);   
+        });            
     },
 
     logMessageDelete: function(bot, message) {
-        var loggingChannel = this.getLoggingChannel(message.guild);
-        loggingChannel.sendMessage(`:x: **Message Deleted** :x:
-                                    \n**User**: ${message.author.username}
-                                    \n**Message**: ${message.content}`);
+        this.getLoggingChannel(message.guild, function(loggingChannel) {
+            loggingChannel.sendMessage(`:x: **Message Deleted** :x:
+                                       \n**User**: ${message.author.username}
+                                       \n**Message**: ${message.content}`);    
+        });        
     },
 
-    logMessageUpdate: function(bot, oldMessage, newMessage){
-        var loggingChannel = this.getLoggingChannel(newMessage.guild);
-        loggingChannel.sendMessage(`:pencil2: **Message Updated** :pencil2:
-                                    \n**User**: ${newMessage.author.username}
-                                    \n**Old Message**: ${oldMessage.content}
-                                    \n**New Message**: ${newMessage.content}`);
+    logMessageUpdate: function(bot, oldMessage, newMessage){                
+        this.getLoggingChannel(oldMessage.guild, function(loggingChannel) {
+            loggingChannel.sendMessage(`:pencil2: **Message Updated** :pencil2:
+                                        \n**User**: ${newMessage.author.username}
+                                        \n**Old Message**: ${oldMessage.content}
+                                        \n**New Message**: ${newMessage.content}`);            
+        });
     },
 
-    getLoggingChannel: function(guild){
+    getLoggingChannel: function(guild, callback){                
         var loggingChannel = guild.channels.find('name', 'mannersbot-logging');
-        if(loggingChannel === null){
-            message.guild.createChannel("mannersbot-logging", "text")
-            .then(loggingChannel => { return loggingChannel });
+        var override = {
+            id: guild.id,
+            type: 'role',
+            allow: 1024,
+            deny: 2048
+        };
+        
+        if(loggingChannel == null){                        
+            guild.createChannel('mannersbot-logging', 'text', [override])
+            .then(newChannel => callback(newChannel))
+            .catch(console.error);
         }
         else {
-            return loggingChannel;
-        }
+            callback(loggingChannel);
+        }                   
     }
 }
