2b114e85a631e1d3d983411563758707162b0da7
msciotti <mason.sciotti@gmail.com>
2018-11-05 18:08:22 -0800

    configurable warning numbers

--- bot.js
+++ bot.js
@@ -1,39 +1,38 @@
-var Punishment = require('./punishment.js');
-var Logging = require('./logging.js');
-var Initialize = require('./initialize.js');
+const Punishment = require('./punishment.js');
+const Logging = require('./logging.js');
+const Initialize = require('./initialize.js');
 
 const Discord = require('discord.js');
 const settings = require('./settings.json');
 const bot = new Discord.Client();
-var bannedWords = Initialize.loadBannedWords();
+const bannedWords = Initialize.loadBannedWords();
 
 bot.on('ready', () => {
-    console.log('I am ready to make your server a better place!')
+  console.log('I am ready to make your server a better place!');
 });
 
 bot.on('message', message => {
-    if(message.channel.type != "dm") {
-        if(!Punishment.checkPermissions(message.member)){
-            if(Punishment.checkProfanity(message.content, bannedWords)){
-                message.delete()
-                .then(msg => {                    
-                    Punishment.doleOutPunishment(bot, msg.member, msg.guild);
-                })
-            }
-        } 
-    }      
+  if (
+    message.author != bot.user &&
+    message.channel.type != 'dm' &&
+    Punishment.checkProfanity(message.content, bannedWords)
+  ) {
+    message.delete().then(msg => {
+      Punishment.doleOutPunishment(bot, msg.member, msg.guild);
+    });
+  }
 });
 
 bot.on('messageDelete', message => {
-    Logging.logMessageDelete(bot, message);
-})
+  Logging.logMessageDelete(message);
+});
 
 bot.on('messageUpdate', (oldMessage, newMessage) => {
-    Logging.logMessageUpdate(bot, oldMessage, newMessage);
-})
+  Logging.logMessageUpdate(oldMessage, newMessage);
+});
 
-bot.on('guildBanAdd', (guild, user) =>{
-    Logging.logUserBan(bot, user, guild);
-})
+bot.on('guildBanAdd', (guild, user) => {
+  Logging.logUserBan(user, guild);
+});
 
 bot.login(settings['BOT_LOGIN_TOKEN']);
--- punishment.js
+++ punishment.js
@@ -1,42 +1,38 @@
 var bannedUsers = {};
+const MAX_WARNINGS = 1;
 
 module.exports = {
-    doleOutPunishment: function(bot, member, guild) {
-        if(bannedUsers[guild.id] == null){            
-            bannedUsers[guild.id] = [];                 
-        }
+  doleOutPunishment: function(bot, member, guild) {
+    if (!bannedUsers[guild.id]) {
+      bannedUsers[guild.id] = {};
+    }
+    var warnings = bannedUsers[guild.id][member.id];
+    if (!warnings) {
+      this.warnUser(bot, member);
+      bannedUsers[guild.id][member.id] = 1;
+    } else if (warnings < MAX_WARNINGS) {
+      this.warnUser(bot, member);
+      bannedUsers[guild.id][member.id]++;
+    } else if (warnings == MAX_WARNINGS) {
+      this.banUser(member);
+      bannedUsers[guild.id][member.id] = 0;
+    }
+  },
 
-        var index = bannedUsers[guild.id].indexOf(member.id);        
-        if(index > -1) {
-            bannedUsers[guild.id].splice(index, 1);
-            this.banUser(bot, member, guild)
-        }
-        else {
-            bannedUsers[guild.id].push(member.id);
-            this.warnUser(bot, member);
-        }                     
-    },
+  banUser: function(member) {
+    member.user.send('You have been banned for posting profanity.');
+    member.ban(7).catch(console.error);
+  },
 
-    banUser: function(bot, member, guild) {
-        member.user.dmChannel.sendMessage('You have been banned for posting profanity.')              
-        member.ban(7);
-    },
+  warnUser: function(bot, member) {
+    bot.fetchUser(member.id).then(user => user.send('Your message has been deleted for profanity and logged.'));
+  },
 
-    warnUser: function(bot, member) {    
-        bot.fetchUser(member.id)
-        .then(user => user.sendMessage('Your message has been deleted for profanity and logged.'))   
-    },
-
-    checkPermissions: function(member) {    
-        return member.hasPermission("MANAGE_MESSAGES", false);        
-    },
-
-    checkProfanity: function(message, bannedWords) {
-        var words = message.split(" ");
-        for(var word of words){
-            if(bannedWords.indexOf(word) > -1)
-                return true;
-        }
-        return false;
-    }              
-}
+  checkProfanity: function(message, bannedWords) {
+    var words = message.split(' ');
+    for (var word of words) {
+      if (bannedWords.indexOf(word) > -1) return true;
+    }
+    return false;
+  },
+};
